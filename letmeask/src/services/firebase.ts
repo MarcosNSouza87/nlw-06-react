dae6d3834c1d4ef6406ee77dd1bcdc25d6b7a524
MarcosNSouza87 <m_souza@outlook.com.br>
2021-06-25 14:37:11 -0300

    feat: aula 1 e 2

--- letmeask/src/services/firebase.ts
+++ letmeask/src/services/firebase.ts
@@ -3,24 +3,16 @@ import firebase from "firebase";
 import "firebase/auth";
 import "firebase/database";
 
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_API_KEY,
-//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-//   databaseURL: process.env.REACT_APP_DATABASE_URL,
-//   projectId: process.env.REACT_APP_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-//   appId: process.env.REACT_APP_APP_ID,
-// };
 const firebaseConfig = {
-  apiKey: "AIzaSyBOiezN_XTXcFNQkwyM-o3GtT66vkmyTvE",
-  authDomain: "letmeask-b1da4.firebaseapp.com",
-  databaseURL: "https://letmeask-b1da4-default-rtdb.firebaseio.com",
-  projectId: "letmeask-b1da4",
-  storageBucket: "letmeask-b1da4.appspot.com",
-  messagingSenderId: "549988659939",
-  appId: "1:549988659939:web:e4aeb0c615564a87e30bd6"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
+
 firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
@@ -28,4 +20,4 @@ const database = firebase.database();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 
-export { firebase, auth, database,provider,firebaseConfig };
+export { firebase, auth, database, provider, firebaseConfig };
